1c0beb44e0d709212c98ec267de06d8f60cc6b36
Rakshit-Taksande <58409317+Rakshit-Taksande@users.noreply.github.com>
2022-03-04 00:58:21 +0530

    Add files via upload

---
+++ Day8/FindDigits.java
@@ -0,0 +1,123 @@
+/**
+An integer  is a divisor of an integer  if the remainder of .
+
+Given an integer, for each digit that makes up the integer determine whether it is a divisor. Count the number of divisors occurring within the integer.
+
+Example
+
+Check whether ,  and  are divisors of . All 3 numbers divide evenly into  so return .
+
+
+Check whether , , and  are divisors of . All 3 numbers divide evenly into  so return .
+
+
+Check whether  and  are divisors of .  is, but  is not. Return .
+
+Function Description
+
+Complete the findDigits function in the editor below.
+
+findDigits has the following parameter(s):
+
+int n: the value to analyze
+Returns
+
+int: the number of digits in  that are divisors of 
+Input Format
+
+The first line is an integer, , the number of test cases.
+The  subsequent lines each contain an integer, .
+
+Constraints
+
+
+
+Sample Input
+
+2
+12
+1012
+Sample Output
+
+2
+3
+
+ */
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'findDigits' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int findDigits(int n) {
+    // Write your code here
+    String numberToString = String.valueOf(n);
+        char[] numberArray = numberToString.toCharArray();
+        int NoOfDigits = 0;
+        for (char x:numberArray) {
+            int val = (int) x - '0';
+            if (val == 0)
+             continue;
+            if (n % val == 0) {
+                NoOfDigits ++;
+            }
+        }
+        return NoOfDigits;
+    }
+
+}
+
+public class FindDigits {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int result = Result.findDigits(n);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
+/**
+Input (stdin):-
+2
+12
+1012
+
+Your Output (stdout):-
+2
+3
+
+Expected Output:-
+2
+3
+ */
---
+++ Day8/ViralAdvertising.java
@@ -0,0 +1,106 @@
+/**
+HackerLand Enterprise is adopting a new viral advertising strategy. When they launch a new product, they advertise it to exactly  people on social media.
+
+On the first day, half of those  people (i.e., ) like the advertisement and each shares it with  of their friends. At the beginning of the second day,  people receive the advertisement.
+
+Each day,  of the recipients like the advertisement and will share it with  friends on the following day. Assuming nobody receives the advertisement twice, determine how many people have liked the ad by the end of a given day, beginning with launch day as day .
+
+Example
+.
+
+Day Shared Liked Cumulative
+1      5     2       2
+2      6     3       5
+3      9     4       9
+4     12     6      15
+5     18     9      24
+The progression is shown above. The cumulative number of likes on the  day is .
+
+Function Description
+
+Complete the viralAdvertising function in the editor below.
+
+viralAdvertising has the following parameter(s):
+
+int n: the day number to report
+Returns
+
+int: the cumulative likes at that day
+Input Format
+
+A single integer, , the day number.
+
+Constraints
+
+Sample Input
+
+3
+Sample Output
+
+9
+ */
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'viralAdvertising' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int viralAdvertising(int n) {
+    // Write your code here
+        int count=0;
+        int shared=5; 
+        int liked;
+        for(int day=1;day<=n;day++)
+        {
+            liked=(shared/2);
+            shared=liked*3;
+            count+=liked;
+        }
+        return count;
+    }
+    }
+
+
+
+public class ViralAdvertising {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.viralAdvertising(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
+/**
+Input (stdin):-
+3
+
+Your Output (stdout):-
+9
+
+Expected Output:-
+9
+ */ 
